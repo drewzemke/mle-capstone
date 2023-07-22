6d3e329a0377901ea3cb8a756d0607e9d4a88a83
Drew Zemke <dnzemke@ucdavis.edu>
2023-07-22 13:05:40 -0700

    react-onnx: handles varying aspect ratios

--- experiments/react-onnx/src/App.tsx
+++ experiments/react-onnx/src/App.tsx
@@ -1,12 +1,22 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import cv from "@techstark/opencv-js";
 import * as ort from "onnxruntime-web";
 import "./App.css";
 import { Tensor } from "onnxruntime-web";
 import { labels } from "./labels";
 
+type Size = {
+  width: number;
+  height: number;
+};
+
+// the image size that the model expects
+const MODEL_SIZE = 640;
+
+// 1 image, 3 channels, width and height
+const modelInputShape = [1, 3, MODEL_SIZE, MODEL_SIZE];
+
 // TODO: learn more about what these mean...
-const modelInputShape = [1, 3, 640, 640];
 const topk = 100;
 const iouThreshold = 0.45;
 const scoreThreshold = 0.25;
@@ -17,11 +27,11 @@ async function getImageTensor(canvas: HTMLCanvasElement) {
   const imgMat = cv.imread(canvas);
   const mat = new cv.Mat(imgMat.rows, imgMat.cols, cv.CV_8UC3);
   cv.cvtColor(imgMat, mat, cv.COLOR_RGBA2BGR);
-  cv.resize(mat, mat, new cv.Size(640, 640));
+  cv.resize(mat, mat, new cv.Size(MODEL_SIZE, MODEL_SIZE));
   const input = cv.blobFromImage(
     mat,
     1 / 255.0,
-    new cv.Size(640, 640),
+    new cv.Size(MODEL_SIZE, MODEL_SIZE),
     new cv.Scalar(0, 0, 0),
     true,
     false
@@ -36,19 +46,32 @@ async function getImageTensor(canvas: HTMLCanvasElement) {
   return tensor;
 }
 
-function processResults(output: ort.Tensor) {
-  const whatDoTheseBoxesMean = [];
+type Box = { centerX: number; centerY: number; width: number; height: number };
+type LabeledBox = { box: Box; label: string; score: number };
+
+function processResults(output: ort.Tensor, canvasSize: Size) {
+  const whatDoTheseBoxesMean: LabeledBox[] = [];
+  const ratioX = MODEL_SIZE / canvasSize.width;
+  const ratioY = MODEL_SIZE / canvasSize.height;
+
   for (let index = 0; index < output.dims[1]; index++) {
     const row = output.data.slice(
       index * output.dims[2],
       (index + 1) * output.dims[2]
     ) as Float32Array;
-    const box = row.slice(0, 4);
+    const modelBox = row.slice(0, 4);
     const scores = row.slice(4, 4 + numClasses);
     const score = Math.max(...scores);
 
+    const [x, y, w, h] = modelBox;
+    const centerX = x / ratioX;
+    const centerY = y / ratioY;
+    const width = w / ratioX;
+    const height = h / ratioY;
+    const scaledBox = { centerX, centerY, width, height };
+
     const label = scores.indexOf(score);
-    whatDoTheseBoxesMean.push({ box, score, label: labels[label] });
+    whatDoTheseBoxesMean.push({ box: scaledBox, score, label: labels[label] });
   }
 
   return whatDoTheseBoxesMean;
@@ -59,16 +82,18 @@ async function runInference(
   nms: ort.InferenceSession,
   tensor: ort.TypedTensor<"float32">
 ) {
-  const config = new Tensor(
+  const nmsConfig = new Tensor(
     "float32",
     new Float32Array([topk, iouThreshold, scoreThreshold])
   );
   const { output0 } = await yolo.run({ images: tensor });
-  const result = await nms.run({ detection: output0, config });
-  return result;
+  const { selected } = await nms.run({ detection: output0, config: nmsConfig });
+  return selected;
 }
 
 async function predict(canvas: HTMLCanvasElement) {
+  console.log("starting inference...");
+  const startTime = new Date();
   // 1. convert to tensor
   const imageTensor = await getImageTensor(canvas);
 
@@ -77,18 +102,33 @@ async function predict(canvas: HTMLCanvasElement) {
   const nms = await ort.InferenceSession.create("nms-yolov8.onnx");
 
   // 3. process results
-  const { selected } = await runInference(yolo, nms, imageTensor);
-  const results = processResults(selected);
+  const inferenceResults = await runInference(yolo, nms, imageTensor);
+  const canvasSize: Size = { width: canvas.width, height: canvas.height };
+  const results = processResults(inferenceResults, canvasSize);
 
   // 4. return predictions
+  const endTime = new Date();
+  console.log(
+    `inference complete in ${endTime.getTime() - startTime.getTime()} ms`
+  );
   return results;
 }
+
+const CANVAS_MAX_SIZE = 800;
 
 function App() {
   const canvas = useRef<HTMLCanvasElement>(null!);
 
   const image = new Image();
-  image.src = "pupper.png";
+  image.src = "pupper4.jpg";
+
+  const imageToCanvasRatio =
+    Math.max(image.width, image.height) / CANVAS_MAX_SIZE;
+
+  const canvasSize: Size = {
+    width: image.width / imageToCanvasRatio,
+    height: image.height / imageToCanvasRatio,
+  };
 
   const [initialized, setInitialized] = useState<boolean>(false);
   cv.onRuntimeInitialized = () => {
@@ -102,15 +142,20 @@ function App() {
         return;
       }
 
-      ctx.drawImage(image, 0, 0, 640, 640);
+      ctx.drawImage(image, 0, 0, canvasSize.width, canvasSize.height);
       predict(canvas.current).then((results) => {
         results.forEach(({ box, label }) => {
           console.log(box);
           console.log(label);
-          const [x, y, w, h] = [box[0], box[1], box[2], box[3]];
-          ctx.strokeStyle = "green";
+          const { centerX, centerY, width, height } = box;
+          ctx.strokeStyle = "orange";
           ctx.lineWidth = 2;
-          ctx.strokeRect(x - w / 2, y - h / 2, w, h);
+          ctx.strokeRect(
+            centerX - width / 2,
+            centerY - height / 2,
+            width,
+            height
+          );
         });
       });
     }
@@ -122,7 +167,11 @@ function App() {
       <div>
         <p>Let's find out. </p>
       </div>
-      <canvas ref={canvas} width={640} height={640} />
+      <canvas
+        ref={canvas}
+        width={canvasSize.width}
+        height={canvasSize.height}
+      />
     </>
   );
 }
